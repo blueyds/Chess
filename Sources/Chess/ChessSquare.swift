07e60f6ff6958c4110d75ec93b45170762cf70fc
Blueyds <9055449+blueyds@users.noreply.github.com>
2026-06-28 01:21:36 -0500

    error

--- Sources/Chess/ChessSquare.swift
+++ Sources/Chess/ChessSquare.swift
@@ -14,46 +14,50 @@ public struct ChessSquare: Identifiable{
 	public init(at coord: Coordinates){
 		self.coordinates = coord
 		self.piece = nil
-		if ChessSquare.whites.contains(coord){
+		if Self.isWhite(coord){
 			self.color = .white
 		} else {
-			self.color = .black
+			aelf.color = .black
 		}
 	}
 
-	internal static let whites: Set<Coordinates> = [
-		Coordinates(file: .b, rank: .one),
-		Coordinates(file: .d, rank: .one),
-		Coordinates(file: .f, rank: .one),
-		Coordinates(file: .h, rank: .one),
-		Coordinates(file: .a, rank: .two),
-		Coordinates(file: .c, rank: .two),
-		Coordinates(file: .e, rank: .two),
-		Coordinates(file: .g, rank: .two),
-		Coordinates(file: .b, rank: .three),
-		Coordinates(file: .d, rank: .three),
-		Coordinates(file: .f, rank: .three),
-		Coordinates(file: .h, rank: .three),
-		Coordinates(file: .a, rank: .four),
-		Coordinates(file: .c, rank: .four),
-		Coordinates(file: .e, rank: .four),
-		Coordinates(file: .g, rank: .four),
-		Coordinates(file: .b, rank: .five),
-		Coordinates(file: .d, rank: .five),
-		Coordinates(file: .f, rank: .five),
-		Coordinates(file: .h, rank: .five),
-		Coordinates(file: .a, rank: .six),
-		Coordinates(file: .c, rank: .six),
-		Coordinates(file: .e, rank: .six),
-		Coordinates(file: .g, rank: .six),
-		Coordinates(file: .b, rank: .seven),
-		Coordinates(file: .d, rank: .seven),
-		Coordinates(file: .f, rank: .seven),
-		Coordinates(file: .h, rank: .seven),
-		Coordinates(file: .a, rank: .eight),
-		Coordinates(file: .c, rank: .eight),
-		Coordinates(file: .e, rank: .eight),
-		Coordinates(file: .g, rank: .eight)
+	private static func isWhite(_ coordinate: Coordinates) [
+		switch coordinate{
+			case Coordinates(file: .b, rank: .one),
+				Coordinates(file: .d, rank: .one),
+				Coordinates(file: .f, rank: .one),
+				Coordinates(file: .h, rank: .one),
+				Coordinates(file: .a, rank: .two),
+				Coordinates(file: .c, rank: .two),
+				Coordinates(file: .e, rank: .two),
+				Coordinates(file: .g, rank: .two),
+				Coordinates(file: .b, rank: .three),
+				Coordinates(file: .d, rank: .three),
+				Coordinates(file: .f, rank: .three),
+				Coordinates(file: .h, rank: .three),
+				Coordinates(file: .a, rank: .four),	
+				Coordinates(file: .c, rank: .four),
+				Coordinates(file: .e, rank: .four),
+				Coordinates(file: .g, rank: .four),
+				Coordinates(file: .b, rank: .five),
+				Coordinates(file: .d, rank: .five),
+				Coordinates(file: .f, rank: .five),
+				Coordinates(file: .h, rank: .five),
+				Coordinates(file: .a, rank: .six),
+				Coordinates(file: .c, rank: .six),
+				Coordinates(file: .e, rank: .six),
+				Coordinates(file: .g, rank: .six),
+				Coordinates(file: .b, rank: .seven),
+				Coordinates(file: .d, rank: .seven),
+				Coordinates(file: .f, rank: .seven),
+				Coordinates(file: .h, rank: .seven),
+				Coordinates(file: .a, rank: .eight),
+				Coordinates(file: .c, rank: .eight),
+				Coordinates(file: .e, rank: .eight),
+				Coordinates(file: .g, rank: .eight):
+				return true
+			default: return false
+			}		
 	]
 
 }
